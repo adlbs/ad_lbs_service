92d5185674e8ba8354094e6b9319577e4ab801db
adlbs <ad_lds@163.com>
2014-09-29 19:03:58 +0800

    WS
    WebService

---
+++ LBS/WS_time/IServiceAction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Web;
+
+namespace LBS
+{
+    public interface IServiceAction 
+    {
+        /*
+         *接口名称类似JAVA编写语法首字母小写其余单词首字母大写专用名词全部大写 
+         *get开头表示从服务器获取数据(有数据返回Of后面代表返回的内容形式没有规定则返回默认接口指定类型)
+         *set开头代表让服务执行某个操作(一般为SQL),一般不返回实质性内容(带有Of的与get带有of的规则一致)
+         *实例1 getADListByTypeOfXML(int,int)表示从服务以广告类别获取广告列表接口定义返回值为string
+         *实质返回的内容为XML结构的文档,便于客户端的数据解析(SAX,Pull,Dom)
+         *实例2 setSQLOfXML(string)表示让服务执行一条SQL语句并且返回具有XML文档结构的文档不带Of的表示
+         *只返回SQL语句的执行成功与否其值为"True" or "False"
+         *在本接口定义中基本所有接口的显式返回值为string,其具体意义可能会是Bool,XML等等的类型.
+         *主要是考虑到类型通用,以及数据的传输,DataSet是为了方便PC管理系统的开发.下面每个接口会有详细介绍
+         */
+        //用于注册一个新用户type代表用户类型,并返回注册是否成功
+        string register(string username, string userpassword, int type);
+        //登录服务器,并返回登录是否成功
+        string login(string username, string userpassword);
+        //以广告类型获取广告列表,并返回具有XML文档结构的字符串
+        string getADListByTypeOfXML(int adtype, int hobtype);
+        //以用户名获取广告列表,并返回具有XML文档结构的字符串
+        string getADListByUserNameOfXML(string username);
+        //以地理位置获取广告列表,并返回具有XML文档结构的字符串
+        string getADListByAddressOfXML(string address);
+        //获取广告类型排行榜,并返回具有XML文档结构的字符串
+        string getRankToNOfXML(int type, int number);
+        //以广告的Id获取广告的详细信息,并返回具有XML文档结构的字符串
+        string getADByIdOfXML(int id);
+        //让服务执行一条SQL语句,并返回是否执行成功("True" or "False")
+        string setSQL(string sql);
+        //让服务执行一条SQL语句,并返回DataSet(数据集)结构的对象
+        DataSet setSQLOfDataSet(string sql);
+        //让服务执行一条SQL语句,并返回具有XML文档结构的字符串
+        string setSQLOfXML(string sql);
+        //以广告ID删除广告,并返回是否成功
+        string setDeleteADById(int id);
+        //以用户名删除广告,并返回是否成功
+        string setDeleteADByUserName(string username);
+        
+
+    }
+}
---
+++ LBS/WS_time/Service.asmx.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using PELessons.DAL;
+namespace LBS
+{
+    /// <summary>
+    /// Service1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://intelligentfreestyle.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。
+    // [System.Web.Script.Services.ScriptService]
+    public class LBS : System.Web.Services.WebService
+    {
+        static string strcon = @"Server=172.16.3.109;Database=LbsDB ;uid=admin888;pwd=lbsadmin";
+
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "登录服务器")]
+        public string login(string email, string userpassword)
+        {
+
+            
+            try
+            {
+
+                string sql = "Select u_ID from UsersInfo where u_Email=\'" + email.Trim() + "\' and u_Password=\'" + userpassword.Trim() + "\'" + " ;";
+
+                int f = (int)SQLHelper.ExecuteScalar(sql, CommandType.Text);
+               
+                if (f > 0)
+                    return true.ToString();
+                else
+                    return false.ToString();
+            }
+            catch (Exception )
+            {
+                return false.ToString();
+            }
+
+        }
+        
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "以广告类别获取广告列表")]
+        public string getADListByType(string t_ID, string ad_ContentType)
+        {
+            try
+            {
+                string sql = "select ADInfo.ad_ID,ADInfo.ad_Title,ADInfo.t_ID,ADInfo.ad_ContentType,ADInfo.ad_CreateTime from ADInfo where ADInfo.t_ID=\'"+t_ID.Trim()+"\' and ad_ContentType=\'"+ad_ContentType.Trim()+"\'";//查询广告的Sql语句
+                DataSet ds = new DataSet();
+                ds = SQLHelper.ExecuteDataSet(sql, CommandType.Text);
+                return ds.GetXml();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "以商家ID称获取广告列表")]
+        public string getADListByADS_ID(string ads_ID)
+        {
+            try
+            {
+                string sql = "select ADInfo.ad_ID,ADInfo.ad_Title,ADInfo.t_ID,ADInfo.ad_ContentType,ADInfo.ad_CreateTime from ADInfo where ADInfo.ads_ID=\'" + ads_ID.Trim() + "\' ";
+                DataSet ds = new DataSet();
+                ds = SQLHelper.ExecuteDataSet(sql, CommandType.Text);
+                return ds.GetXml();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "以广告投放时间获取广告列表")]
+        public string getADListByDate(string date)
+        {
+            try
+            {
+                string sql = "select ADInfo.ad_ID,ADInfo.ad_Title,ADInfo.t_ID,ADInfo.ad_ContentType,ADInfo.ad_CreateTime from ADInfo where ADInfo.ad_CreateTime=\'" + date.Trim() + "\' ";//查询广告的Sql语句
+                DataSet ds = new DataSet();
+                ds = SQLHelper.ExecuteDataSet(sql, CommandType.Text);
+                return ds.GetXml();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+       
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "以广告ID获取广告详情")]
+        public string getADById(int id)
+        {
+            try
+            {
+                string sql = "select * from ADInfo where ADInfo.ad_ID=\'"+id.ToString()+"\'";//SQL语句
+                DataSet ds;
+                ds = SQLHelper.ExecuteDataSet(sql, CommandType.Text);
+                return ds.GetXml();
+            }
+            catch (Exception)
+            { 
+                return "";
+            }
+            
+           
+
+        }
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "执行一条指定SQL只返回执行是否成功")]
+        public string setSQL(string sql)
+        {
+            SqlConnection lo_conn = new SqlConnection(strcon);
+            try
+            {
+                lo_conn.Open();
+            }
+            catch (Exception )
+            {
+                return false.ToString();
+            }
+            try
+            {
+                SqlCommand Sqlcom = new SqlCommand();
+                Sqlcom.Connection = lo_conn;
+                Sqlcom.CommandText = sql;
+                Sqlcom.ExecuteScalar();
+                lo_conn.Close();
+                lo_conn.Dispose();
+                return true.ToString();
+            }
+            catch (Exception )
+            {
+                return false.ToString();
+            }
+        }
+        private string FindNumber(string text)
+        {
+            string result = "";
+            foreach (char c in text)
+                if (c >= 48 && c <= 57)
+                    result += c;
+            return result;
+        }
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "注册新用户  目前只是模型,具体不一定这么写")]
+        public string register(string username, string userpassword, string email)
+        { 
+             
+            try
+            {
+
+                string sql = @"insert into UsersInfo(,u_ID,u_Nickname,u_Password,u_Email) values(\'"+FindNumber(DateTime.Now.ToString())+"\',\'" + username.Trim() + "\',\'" + userpassword.Trim() + "\'," + email.Trim() + "\');";
+                if(SQLHelper.ExecuteNonQuery(sql, CommandType.Text)>0)
+                    return true.ToString();
+                else
+                    return false.ToString();
+            }
+            catch (Exception)
+            {
+                return false.ToString();
+            }
+           
+        }
+       
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "执行一条指定SQL只返回执行结果-DataSet")]
+        public DataSet setSQLOfDataSet(string sql)
+        {
+            SqlConnection lo_conn = new SqlConnection(strcon);
+            try
+            {
+                lo_conn.Open();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            try
+            {
+                SqlCommand Sqlcom = new SqlCommand();
+                Sqlcom.Connection = lo_conn;
+                Sqlcom.CommandText = sql;
+                SqlDataAdapter dbAdapter = new SqlDataAdapter(Sqlcom);
+                DataSet ds = new DataSet();
+                dbAdapter.Fill(ds);
+               
+                lo_conn.Close();
+                lo_conn.Dispose();
+                return ds;
+               
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        //--------------------------------------------------------------------------------------------
+        [WebMethod(Description = "执行一条指定SQL只返回执行结果-XML")]
+        public string setSQLOfXML(string sql)
+        {
+            SqlConnection lo_conn = new SqlConnection(strcon);
+            try
+            {
+                lo_conn.Open();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+            try
+            {
+                SqlCommand Sqlcom = new SqlCommand();
+                Sqlcom.Connection = lo_conn;
+                Sqlcom.CommandText = sql;
+                
+                SqlDataAdapter dbAdapter = new SqlDataAdapter(Sqlcom);
+                DataSet ds = new DataSet();
+                dbAdapter.Fill(ds);
+                string xml = ds.GetXml();
+                lo_conn.Close();
+                lo_conn.Dispose();
+                return xml;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
